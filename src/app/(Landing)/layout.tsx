43b5b5cbc3014db154e4a02f782b69eed65c33b5
omarwael5556666 <omarwael5556666@gmail.com>
2025-12-10 00:07:39 +0200

    chore: update landing title to 'Omar Dev Portfolio' and manifest branding

--- src/app/(Landing)/layout.tsx
+++ src/app/(Landing)/layout.tsx
@@ -2,8 +2,8 @@ import { LandingLayout } from '@/components/layouts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Kimo Dev Portfolio',
-  description: 'Kimo Dev Portfolio',
+  title: 'Omar Dev Portfolio',
+  description: 'Omar Dev Portfolio',
 };
 
 const layout = ({ children }: { children: React.ReactNode }) => {
